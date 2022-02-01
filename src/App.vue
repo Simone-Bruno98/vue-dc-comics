2c2eb17d1f4c5ffec2948ef2b5313b7f7ca74843
Simone-Bruno98 <bruno-simone@outlook.it>
2022-02-01 23:57:59 +0100

    Struttura impostata

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <Header/>
+    <Content/>
+    <Nav/>
+    <Footer1/>
+    <Footer2/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Content from './components/Content.vue'
+import Nav from './components/Nav.vue'
+import Footer1 from './components/Footer1.vue'
+import Footer2 from './components/Footer2.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Content,
+    Nav,
+    Footer1,
+    Footer2,
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
